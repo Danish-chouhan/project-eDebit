0cf134b73a3ca748ecaa180c75b490813499aa62
Danish-chouhan <danishchouhan41@gmail.com>
2022-11-29 20:12:48 +0530

    eDabit-solution

---
+++ array/convert-all-array-items-to-string.js
@@ -0,0 +1,18 @@
+// Create a function that takes an array of integers and strings. Convert integers to strings and return the new array.
+
+// Examples
+// parseArray([1, 2, "a", "b"]) ➞ ["1", "2", "a", "b"]
+
+// parseArray(["abc", 123, "def", 456]) ➞ ["abc", "123", "def", "456"]
+
+// parseArray([1, 2, 3, 17, 24, 3, "a", "123b"]) ➞ ["1", "2", "3", "17", "24", "3", "a", "123b"]
+
+// parseArray([]) ➞ []
+// Notes
+// Return [] if array is empty.
+
+function parseArray(arr) {
+arr = [1,2,"a","b"]
+return arr.map(String) 
+}
+console.log(parseArray())
