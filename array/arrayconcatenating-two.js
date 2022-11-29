315d0815d0a2db87e4e524d0b6415e6b7255f033
Danish-chouhan <danishchouhan41@gmail.com>
2022-11-29 19:52:30 +0530

    eDabit-solution

---
+++ array/arrayconcatenating-two.js
@@ -0,0 +1,15 @@
+// Create a function to concatenate two integer arrays.
+
+// Examples
+// concat([1, 3, 5], [2, 6, 8]) ➞ [1, 3, 5, 2, 6, 8]
+
+// concat([7, 8], [10, 9, 1, 1, 2]) ➞ [7, 8, 10, 9, 1, 1, 2]
+
+// concat([4, 5, 1], [3, 3, 3, 3, 3]) ➞ [4, 5, 1, 3, 3, 3, 3, 3]
+// Notes
+// Don't forget to return the result.
+// See Resources tab for more info.
+var a = [1,3,5]
+var b = [2,6,8]
+var c = a.concat(b)
+console.log(c)
