bf69f148918916f5440851148d21afecbd85295e
Danish-chouhan <danishchouhan41@gmail.com>
2022-11-29 21:31:43 +0530

    eDabit-solution

---
+++ array/convert-an-array-to-string.js
@@ -0,0 +1,14 @@
+// Create a function that takes an array of numbers or letters and returns a string.
+
+// Examples
+// arrayToString([1, 2, 3, 4, 5, 6]) ➞ "123456"
+
+// arrayToString(["a", "b", "c", "d", "e", "f"]) ➞ "abcdef"
+
+// arrayToString([1, 2, 3, "a", "s", "dAAAA"]) ➞ "123asdAAAA"
+// Notes
+// N/A
+
+var a = [1,2,3,4,5,6];
+var b = a.toString();
+console.log(b);
