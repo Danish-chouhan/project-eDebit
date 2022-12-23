524c367c3249b71b88f5b29ec90ee217572f6224
Danish-chouhan <danishchouhan41@gmail.com>
2022-12-23 10:47:25 +0530

    edabit-solution

---
+++ array/find-the-index-part-2.js
@@ -0,0 +1,17 @@
+// Create a function that searches for the index of a given item in an array. If the item is present, it should return the index, otherwise, it should return -1.
+
+// Examples
+// search([1, 2, 3, 4], 3) ➞ 2
+
+// search([2, 4, 6, 8, 10], 8) ➞ 3
+
+// search([1, 3, 5, 7, 9], 11) ➞ -1
+// Notes
+// If the item is not present, return -1.
+// The given array is ordered.
+
+function findTheIndex(x){
+    x = [1,3,5,7,9]
+    return x.indexOf(11)
+}
+document.write(findTheIndex())
