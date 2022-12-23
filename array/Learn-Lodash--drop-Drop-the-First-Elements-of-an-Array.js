f21537655b786eeffa2d4800cb8eda61246c54e6
Danish-chouhan <danishchouhan41@gmail.com>
2022-12-23 10:14:18 +0530

    edabit-solution

---
+++ array/Learn-Lodash--drop-Drop-the-First-Elements-of-an-Array.js
@@ -0,0 +1,15 @@
+var a = [1,2,3]
+var b = a.slice(1)
+document.write(b + "<br>")
+
+var a = [1,2,3]
+var b = a.slice(2)
+document.write(b + "<br>")
+
+var a = [1,2,3]
+var b = a.slice(5)
+document.write(b + "<br>")
+
+var a = [1,2,3]
+var b = a.slice(0)
+document.write(b)
