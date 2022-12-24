5531ee22556e660f564d0629205c413e441a930b
Danish-chouhan <danishchouhan41@gmail.com>
2022-12-24 13:32:06 +0530

    edabit-solution

---
+++ array/square-and-cubes.js
@@ -0,0 +1,24 @@
+// Create a function that takes an array of two numbers and checks if the square root of the first number is equal to the cube root of the second number.
+
+// Examples
+// checkSquareAndCube([4, 8]) ➞ true
+
+// checkSquareAndCube([16, 48]) ➞ false
+
+// checkSquareAndCube([9, 27]) ➞ true
+// Notes
+// Remember to return either true or false.
+// All arrays contain two positive numbers.
+// If you're stuck, check the Resources for a hint.
+
+// var a = Math.sqrt([4])
+// var b = Math.cbrt([8])
+// var c = a === b 
+// document.write(c)
+
+function dan(a,b){
+    a = Math.sqrt[4],
+    b = Math.cbrt[8]
+   return a === b
+}
+document.write(dan())
